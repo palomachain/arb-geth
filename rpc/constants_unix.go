8e77a580a9363794bde54829956419eba006fe64
Christian Lohr <klausklapper@gmail.com>
2023-08-24 11:29:42 +0000

    hack: override unix constants to avoid CGO

--- rpc/constants_unix.go
+++ rpc/constants_unix.go
@@ -19,16 +19,8 @@
 
 package rpc
 
-/*
-#include <sys/un.h>
-
-int max_socket_path_size() {
-struct sockaddr_un s;
-return sizeof(s.sun_path);
-}
-*/
-import "C"
-
 var (
-	max_path_size = C.max_socket_path_size()
+	//  On Linux, sun_path is 108 bytes in size
+	// see http://man7.org/linux/man-pages/man7/unix.7.html
+	max_path_size = 108
 )
